f7d8b4fcb1d1b6cdf8d600206ad42df3d43c2921
Ruan Serra Azul <78482287+RuanSerraAzul@users.noreply.github.com>
2021-12-20 17:35:51 -0300

    Exercicios de 1 ao4 Módulo 2

---
+++ Módulo 2/Exercício 5.py
@@ -0,0 +1,15 @@
+##Escreva um programa para inserir um elemento no inicio de um determinado dicionario ordenado
+DicionarioOrdenado={
+    'color1': 'Red',
+    'color2':'Green',
+    'color3':'Blue'
+    }
+print (DicionarioOrdenado['color1'],"\n")
+
+#Adicionar item ao dicionario
+DicionarioOrdenado['color4'] = 'orange'
+
+
+
+
+print(DicionarioOrdenado)
