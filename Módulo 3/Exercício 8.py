83f844ce671c12eb589854369490505f5d40cfd6
Ruan Serra Azul <78482287+RuanSerraAzul@users.noreply.github.com>
2022-01-26 17:20:56 -0300

    Exercicio 8 finalizado

--- Módulo 3/Exercício 8.py
+++ Módulo 3/Exercício 8.py
@@ -11,7 +11,7 @@ class bombaCombustivel():
         print("A quantidade de combustivel na bomba agora é {}".format(self.quantidadeCombustivel))
 
     def abastecerPorLitro(self,valor):
-        custo = self.valorLitro * valor
+        custo = (self.valorLitro * valor)
         print("Abastecer {0} litros vai custar {1}R$".format(valor,custo))
         self.quantidadeCombustivel -= valor
         print("A quantidade de combustivel na bomba agora é {}".format(self.quantidadeCombustivel))
@@ -19,3 +19,18 @@ class bombaCombustivel():
     def alterarValor(self,valor):
         self.valorLitro = valor
         print("O valor de gasolina pelo litro agora custa {}".format(self.valorLitro))
+
+    def alterarCombustivel(self, combustivel):
+        self.tipoCombustivel = combustivel
+        print("O combustivel na bomba agora é {}".format("combustivel"))
+        
+    def alterarQuantitadeCombustivel(self,valor):
+        self.quantidadeCombustivel = valor
+        print("A quantidade de combstivel na bomba agora é {}".format(self.quantidadeCombustivel))
+
+
+bomb = bombaCombustivel("Gasolina",6.12,500)
+bomb.alterarCombustivel("Diesel")
+bomb.abastecerPorLitro(10)
+bomb.abastecerPorValor(61.20)
+bomb.alterarQuantitadeCombustivel(500)
